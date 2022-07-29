db5c8fedb37a64cd7ca6c1883aa0faeeb55bd995
Frederick-Rico-Hartanto <frederickrico.hartanto@my.jcu.edu.au>
2022-07-29 15:52:10 +0800

    Commit

--- Practical 3/broken_score.py
+++ Practical 3/broken_score.py
@@ -2,15 +2,16 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+import random
 # TODO: Fix this!
 def main():
     score = float(input("Enter score: "))
-    result = determine_score(score)
-
+    random_score = random.randrange(0, 101, 1)
+    result = determine_score(score, random_score)
+    print(f"The random score is {random_score} and the result is {result}")
     print(result)
 
-def determine_score(score):
+def determine_score(score, random_score):
     if score < 0:
         return "Invalid score"
     elif score > 100:
