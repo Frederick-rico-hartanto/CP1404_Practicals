e6349871d2f3d559fa1ff334ce83edb0d0aa08d5
Frederick-Rico-Hartanto <frederickrico.hartanto@my.jcu.edu.au>
2022-07-29 11:48:33 +0800

    Commit

---
+++ Practical 3/temperatures.py
@@ -0,0 +1,36 @@
+
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_to_c(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = convert_to_f(fahrenheit)
+            print("Results: {:.2f} C".format(celsius))
+            # TODO: Write this section to convert F to C and display the result
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            # Remove the "pass" statement when you are done. It's a placeholder.
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_to_c(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_to_f(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+
+main()
