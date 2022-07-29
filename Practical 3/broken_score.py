f6a91a99f563a0dc776f313537ef85238847365e
Frederick-Rico-Hartanto <frederickrico.hartanto@my.jcu.edu.au>
2022-07-29 11:52:43 +0800

    Commit

---
+++ Practical 3/broken_score.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+def main():
+    score = float(input("Enter score: "))
+    result = determine_score(score)
+
+    print(result)
+
+def determine_score(score):
+    if score < 0:
+        return "Invalid score"
+    elif score > 100:
+        return "Invalid score"
+    elif score > 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score < 50:
+        return "Bad"
