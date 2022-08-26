4cd30cace628e289ff82003de6e4328510f273cf
Frederick-rico-hartanto <110209251+Frederick-rico-hartanto@users.noreply.github.com>
2022-08-26 12:56:26 +0800

    Update guitars.py

--- Practical 6/guitars.py
+++ Practical 6/guitars.py
@@ -4,14 +4,14 @@ def main():
     guitars = []
 
     print("My guitars")
-    name = input("Name: ")
+    name = str(input("Name: "))
     while name != "":
         year = int(input("Year: "))
         cost = float(input("Cost: "))
         appending_guitar = Guitar(name, year, cost)
         guitars.append(appending_guitar)
         print(f"{appending_guitar} added.")
-        name = input("Name: ")
+        name = str(input("Name: "))
 
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
