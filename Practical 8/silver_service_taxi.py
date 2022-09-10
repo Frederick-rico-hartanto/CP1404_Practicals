1274471684f183d918da0228efea5f78f0269f3f
Frederick-Rico-Hartanto <frederickrico.hartanto@my.jcu.edu.au>
2022-09-10 18:59:28 +0800

    change to super().get_fare()

---
+++ Practical 8/silver_service_taxi.py
@@ -0,0 +1,17 @@
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    flag_fall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        return "{} plus flag fall of ${:.2f}".format(super().__str__(), self.flag_fall)
+
+    def get_price(self):
+        return self.flag_fall + super().get_fare()
+
+
